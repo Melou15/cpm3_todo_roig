54358d492a0f9c3f7783e167c315d1062024f76c
Melissa Roig <melissa.roig@laposte.net>
2023-10-04 11:07:34 +0200

    style effectué + possibilité de supprimer et valider une tâche

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 function addTask() {
-    var taskInput = document.getElementById("task");
-    var taskText = taskInput.value.trim();
+            var taskInput = document.getElementById("task");
+            var taskText = taskInput.value.trim();
 
-    if (taskText === "") {
-        alert("Veuillez entrer une tâche.");
-        return;
-    }
+            if (taskText === "") {
+                alert("Veuillez entrer une tâche.");
+                return;
+            }
 
-    var taskList = document.getElementById("taskList");
-    var taskItem = document.createElement("li");
-    taskItem.textContent = taskText;
-    taskList.appendChild(taskItem);
+            var taskList = document.getElementById("taskList");
+            var taskItem = document.createElement("li");
 
-    taskInput.value = "";
-}
+            var checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.onchange = function () {
+                toggleTask(checkbox);
+            };
+
+
+            var label = document.createElement("label");
+            label.textContent = taskText;
+
+
+            var deleteButton = document.createElement("button");
+            deleteButton.textContent = "Suppr";
+            deleteButton.onclick = function () {
+                deleteTask(taskItem);
+            };
+
+            taskItem.appendChild(checkbox);
+            taskItem.appendChild(label);
+            taskItem.appendChild(deleteButton);
+
+            taskList.appendChild(taskItem);
+
+            taskInput.value = "";
+        }
+
+        function toggleTask(checkbox) {
+            var label = checkbox.nextElementSibling;
+            label.classList.toggle("completed");
+        }
+
+        function deleteTask(taskItem) {
+            taskItem.remove();
+        }
